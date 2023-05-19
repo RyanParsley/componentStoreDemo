34e7b85779230e3d01ed3e26921861ff7a547198
Ryan Parsley <Ryan.Parsley@JBHunt.com>
2023-05-19 14:47:09 -0400

    decrement ammount on click

--- src/app/grocery-list/grocery-item/grocery-item.component.ts
+++ src/app/grocery-list/grocery-item/grocery-item.component.ts
@@ -3,9 +3,21 @@ import { Grocery } from 'src/app/interfaces/grocery.interface';
 
 @Component({
   selector: 'app-grocery-item',
-  templateUrl: './grocery-item.component.html',
-  styleUrls: ['./grocery-item.component.scss']
+  template: `
+    <div
+      (click)="useItem()"
+      [ngClass]="{ complete: item.amount == 0 }"
+      class="display"
+    >
+      <span>{{ item.name }}</span>
+      <span>{{ item.amount }}</span>
+    </div>
+  `,
+  styleUrls: ['./grocery-item.component.scss'],
 })
 export class GroceryItemComponent {
   @Input() item!: Grocery;
+  public useItem() {
+    this.item.amount--;
+  }
 }
